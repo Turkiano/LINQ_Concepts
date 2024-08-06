20de5725ebdc719f13edc84fe4349ecc24728456
Turkiano <turki.saeed.ksa@gmail.com>
2024-08-06 19:59:08 +0300

    Query Syntax example

--- Program.cs
+++ Program.cs
@@ -1,31 +1,27 @@
-﻿class Program
-{
-    static void Main(string[] args)
-    {
-        Student[] studentArray = { 
-                    new Student() { StudentID = 1, StudentName = "John", age = 18 } ,
-                    new Student() { StudentID = 2, StudentName = "Steve",  age = 21 } ,
-                    new Student() { StudentID = 3, StudentName = "Bill",  age = 25 } ,
-                    new Student() { StudentID = 4, StudentName = "Ram" , age = 20 } ,
-                    new Student() { StudentID = 5, StudentName = "Ron" , age = 31 } ,
-                    new Student() { StudentID = 6, StudentName = "Chris",  age = 17 } ,
-                    new Student() { StudentID = 7, StudentName = "Rob",age = 19  } ,
-                };
+﻿
 
-        // Use LINQ to find teenager students
-        Student[] teenAgerStudents = studentArray.Where(s => s.age > 12 && s.age < 20).ToArray();
-       
-        // Use LINQ to find first student whose name is Bill 
-        Student bill = studentArray.Where(s => s.StudentName == "Bill").FirstOrDefault();
-        
-        // Use LINQ to find student whose StudentID is 5
-        Student student5 = studentArray.Where(s => s.StudentID == 5).FirstOrDefault();
-    }
+
+// string collection (data source)
+IList<string> stringList = new List<string>() { 
+    "C# Tutorials",
+    "VB.NET Tutorials",
+    "Learn C++",
+    "MVC Tutorials" ,
+    "Java" 
+};
 
 
 
-    //Where is the LINQ Executation
+// LINQ Query Syntax
+var result = from s in stringList
+            where s.Contains("Tutorials") 
+            select s;
 
 
-    
-}
+            
+            
+// LINQ Query Executation
+            foreach (var str in result)
+		{
+			Console.WriteLine(str);
+		}
--- obj/Debug/net8.0/LINQ_Concepts.AssemblyInfo.cs
+++ obj/Debug/net8.0/LINQ_Concepts.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LINQ_Concepts")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13a9900bcd0e17d71dcfc909d55ecc4cba634f45")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ffe15f5b15ce57952cb4dbe4fc1acd79547ab626")]
 [assembly: System.Reflection.AssemblyProductAttribute("LINQ_Concepts")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LINQ_Concepts")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
