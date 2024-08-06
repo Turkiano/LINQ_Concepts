4be8c19c6c5eb35cf1ab1c4dcbbd3a185567a489
Turkiano <turki.saeed.ksa@gmail.com>
2024-08-06 22:00:52 +0300

    LINQ Query Syntax 2

--- Program.cs
+++ Program.cs
@@ -1,27 +1,29 @@
 ﻿
 
+namespace LINQ_Query_Syntax;
 
-// string collection (data source)
-IList<string> stringList = new List<string>() { 
-    "C# Tutorials",
-    "VB.NET Tutorials",
-    "Learn C++",
-    "MVC Tutorials" ,
-    "Java" 
-};
-
-
-
-// LINQ Query Syntax
-var result = from s in stringList
-            where s.Contains("Tutorials") 
-            select s;
-
-
-            
-            
-// LINQ Query Executation
-            foreach (var str in result)
-		{
-			Console.WriteLine(str);
+					
+public class Program
+{
+	public static void Main()
+	{
+		// Student collection
+		IList<Student> studentList = new List<Student>() { 
+				new Student() { StudentID = 1, StudentName = "John", Age = 13} ,
+				new Student() { StudentID = 2, StudentName = "Moin",  Age = 21 } ,
+				new Student() { StudentID = 3, StudentName = "Bill",  Age = 18 } ,
+				new Student() { StudentID = 4, StudentName = "Ram" , Age = 20} ,
+				new Student() { StudentID = 5, StudentName = "Ron" , Age = 15 } 
+			};
+		
+		// LINQ Query Syntax to find out teenager students
+		var teenAgerStudent = from s in studentList
+							  where s.Age > 12 && s.Age < 20
+							  select s;
+		Console.WriteLine("Teen age Students:");
+						  
+		foreach(Student std in teenAgerStudent){			
+			Console.WriteLine(std.StudentName);
 		}
+	}
+}
---
+++ Student.cs
@@ -0,0 +1,11 @@
+namespace LINQ_Query_Syntax;
+
+//Student.cs file 
+
+ public class Student{
+
+	public int StudentID { get; set; }
+	public string? StudentName { get; set; }
+	public int Age { get; set; }
+	
+}
