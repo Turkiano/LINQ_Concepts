e4eab7f7130b49fca1127e3f1ea089e135dc27c3
Turkiano <turki.saeed.ksa@gmail.com>
2024-08-07 23:19:53 +0300

    lambad without params

--- Program.cs
+++ Program.cs
@@ -1,20 +1,22 @@
 ﻿using System;
-
 namespace Lambda_Expression;
 
 					
 public class Program
 {
-	delegate bool IsTeenAger(Student stud);
+	delegate bool IsYoungerThan(Student stud, int youngAge);
 	
 	public static void Main()
 	{
-		IsTeenAger isTeenAger = delegate(Student s) { return s.Age > 12 && s.Age < 20; };
+		IsYoungerThan isYoungerThan = (s, youngAge) => {
+			
+			
+			return s.Age < youngAge;
+		};
 		
 		Student stud = new Student() { Age = 25 };
 		
-		Console.WriteLine(isTeenAger(stud));
+		Console.WriteLine(isYoungerThan(stud, 26));
 		
-		
+		}
 	}
-}
