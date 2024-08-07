241b0fb355106b1f8b4067008149ca20e7f94d61
Turkiano <turki.saeed.ksa@gmail.com>
2024-08-07 08:00:29 +0300

    LINQ Method Syntax 01

--- Program.cs
+++ Program.cs
@@ -1,29 +1,30 @@
-﻿
-
-namespace LINQ_Query_Syntax;
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 					
 public class Program
 {
 	public static void Main()
 	{
-		// Student collection
-		IList<Student> studentList = new List<Student>() { 
-				new Student() { StudentID = 1, StudentName = "John", Age = 13} ,
-				new Student() { StudentID = 2, StudentName = "Moin",  Age = 21 } ,
-				new Student() { StudentID = 3, StudentName = "Bill",  Age = 18 } ,
-				new Student() { StudentID = 4, StudentName = "Ram" , Age = 20} ,
-				new Student() { StudentID = 5, StudentName = "Ron" , Age = 15 } 
-			};
+		// string collection
+		IList<string> stringList = new List<string>() { 
+			"C# Tutorials",
+			"VB.NET Tutorials",
+			"Learn C++",
+			"MVC Tutorials" ,
+			"Java" 
+		};
 		
-		// LINQ Query Syntax to find out teenager students
-		var teenAgerStudent = from s in studentList
-							  where s.Age > 12 && s.Age < 20
-							  select s;
-		Console.WriteLine("Teen age Students:");
-						  
-		foreach(Student std in teenAgerStudent){			
-			Console.WriteLine(std.StudentName);
+		// LINQ Method Syntax
+		var result = stringList.Where(s => s.Contains("Tutorials"));
+
+					
+		
+        //LINQ Executation
+		foreach (var str in result)
+		{
+			Console.WriteLine(str);
 		}
 	}
 }
