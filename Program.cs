e5708398ad48daccc0894a2d98fbe3afa07060f3
Turkiano <turki.saeed.ksa@gmail.com>
2024-08-08 11:00:15 +0300

    file is ready

--- Program.cs
+++ Program.cs
@@ -8,12 +8,7 @@ public class Program
 
     public static void Main()
     {
-        Action<Student> PrintStudentDetail = s =>
-        Console.WriteLine("Name: {0}, Age: {1} ", s.StudentName, s.Age);
-
-        Student std = new Student() { StudentName = "Bill", Age = 21 };
-
-        PrintStudentDetail(std);
+       
 
 
     }
