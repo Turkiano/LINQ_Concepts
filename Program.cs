ccd4fbbfb2094a549d67fd66348577b547eb175e
Turkiano <turki.saeed.ksa@gmail.com>
2024-08-08 08:54:41 +0300

    assigning lambda with func delegate

--- Program.cs
+++ Program.cs
@@ -1,31 +1,19 @@
 ﻿using System;
 namespace Lambda_Expression;
 
+
 public class Program
 {
-    delegate bool IsAdult(Student stud);
+
 
     public static void Main()
     {
-        IsAdult isAdult = (s) =>
+        //assigning Lambda to the built-in Delegate (Func)
+        Func<Student, bool> isStudentTeenAger = s => s.Age > 12 && s.Age < 20;
 
-        { 
+        Student stud = new Student() { Age = 21 };
 
-            //statement 01. local variable
-            int adultAge = 18; 
-
-            //statement 02. print a message
-            Console.WriteLine("Lambda expression with multiple statements in the body");
-            
-            //statement 03. check if age is greater or equal 18
-            return s.Age >= adultAge;
-        };
-
-
-
-        Student stud = new Student() { Age = 25 };
-
-        Console.WriteLine(isAdult(stud));
+        Console.WriteLine(isStudentTeenAger(stud));
 
 
     }
--- obj/Debug/net8.0/LINQ_Concepts.AssemblyInfo.cs
+++ obj/Debug/net8.0/LINQ_Concepts.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LINQ_Concepts")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1e3d3962a928664d777f6144bdca98ad767000c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75438185db723ec42c58913625e9732791b70940")]
 [assembly: System.Reflection.AssemblyProductAttribute("LINQ_Concepts")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LINQ_Concepts")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
