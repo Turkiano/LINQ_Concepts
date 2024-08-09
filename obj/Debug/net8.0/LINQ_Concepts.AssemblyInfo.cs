e700b113df9d7f73dbed3554bdbf5ae289a3ca0e
Turkiano <turki.saeed.ksa@gmail.com>
2024-08-09 20:30:51 +0300

    01. lambda expression is ready to push

--- obj/Debug/net8.0/LINQ_Concepts.AssemblyInfo.cs
+++ obj/Debug/net8.0/LINQ_Concepts.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LINQ_Concepts")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10f2736bfa662e625314fb19d07843a5a5fd38bb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b979c7fccf638b029b6a01f221c175318bc2cfe")]
 [assembly: System.Reflection.AssemblyProductAttribute("LINQ_Concepts")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LINQ_Concepts")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
