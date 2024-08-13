e7fad03b067e5721886e0a2418cb84da2b9ebb6e
Turkiano <turki.saeed.ksa@gmail.com>
2024-08-13 09:52:12 +0300

    Where - Query Syntax

--- Program.cs
+++ Program.cs
@@ -1,37 +1,33 @@
-﻿class Program
+﻿//Example: Where clause - LINQ query syntax C#
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Where_Query_Syntax;
+
+public class Program
 {
-    static void Main(string[] args)
+    public static void Main()
     {
-        Student[] studentArray = {
-                    new Student() { StudentID = 1, StudentName = "John", age = 18 } ,
-                    new Student() { StudentID = 2, StudentName = "Steve",  age = 21 } ,
-                    new Student() { StudentID = 3, StudentName = "Bill",  age = 25 } ,
-                    new Student() { StudentID = 4, StudentName = "Ram" , age = 20 } ,
-                    new Student() { StudentID = 5, StudentName = "Ron" , age = 31 } ,
-                    new Student() { StudentID = 6, StudentName = "Chris",  age = 17 } ,
-                    new Student() { StudentID = 7, StudentName = "Rob",age = 19  } ,
-                };
+        // Student collection
+        IList<Student> studentList = new List<Student>() {
+                new Student() { StudentID = 1, StudentName = "John", Age = 13} ,
+                new Student() { StudentID = 2, StudentName = "Moin",  Age = 21 } ,
+                new Student() { StudentID = 3, StudentName = "Bill",  Age = 18 } ,
+                new Student() { StudentID = 4, StudentName = "Ram" , Age = 20} ,
+                new Student() { StudentID = 5, StudentName = "Ron" , Age = 15 }
+            };
 
-        //  LINQ Method syntax to find teenager students
-        Student[] teenAgerStudents = studentArray.Where(s => s.age > 12 && s.age < 20).ToArray();
-
-        //  LINQ Method syntax  to find first student whose name is Bill 
-        Student? bill = studentArray.Where(s => s.StudentName == "Bill").FirstOrDefault();
-
-        //  LINQ Method syntax  to find student whose StudentID is 5
-        Student? student5 = studentArray.Where(s => s.StudentID == 5).FirstOrDefault();
+        // LINQ Query Syntax to find out teenager students
+        var teenAgerStudent = from s in studentList
+                              where s.Age > 12 && s.Age < 20
+                              select s;
+        Console.WriteLine("Teen age Students:");
 
 
-
-        // LINQ Executation
-        foreach (Student filtered in teenAgerStudents)
+        foreach (Student std in teenAgerStudent)
         {
-            Console.WriteLine(filtered.StudentName);
-
+            Console.WriteLine(std.StudentName);
         }
     }
-
 }
-
-
-
--- Student.cs
+++ Student.cs
@@ -1,10 +1,12 @@
 using System;
 
+namespace Where_Query_Syntax;
+
 class Student
 {
     public int StudentID { get; set; }
-    public string StudentName { get; set; }
-    public int age { get; set; }
+    public string? StudentName { get; set; }
+    public int Age { get; set; }
 }
 
 
